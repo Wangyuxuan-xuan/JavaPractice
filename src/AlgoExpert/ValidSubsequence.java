fac928b653a786b7c6c57f733fdadbfa1a9913fe
YuxuanWang566 <97107774+YuxuanWang566@users.noreply.github.com>
2022-04-06 18:33:57 +0200

    Sovled ValidSubsequence

--- src/AlgoExpert/ValidSubsequence.java
+++ src/AlgoExpert/ValidSubsequence.java
@@ -3,28 +3,50 @@ package AlgoExpert;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class ValidSubsequence {
 
     public static void main(String[] args) {
-        List<Integer> array = new ArrayList<>(Arrays.asList(1,2,3,4));
-        List<Integer> sequence =  new ArrayList<>(Arrays.asList(2,3));
+        List<Integer> array = new ArrayList<>(Arrays.asList(1,2,3,4,-1,3,4));
+        List<Integer> sequence =  new ArrayList<>(Arrays.asList(1,2,3,4,-1));
         System.out.println(isValidSubsequence(array,sequence));
     }
 
     public static boolean isValidSubsequence(List<Integer> array, List<Integer> sequence) {
-        // Write your code here.
+
         if (array.size() < sequence.size()){
             return false;
         }
         int i = 0;
         for(Integer integer : array){
-            if(integer.equals(sequence.get(i))){
+            if(i == sequence.size()){
+                break;
+            }
+            if(i < sequence.size() && integer.equals(sequence.get(i))){
                 i++;
-            }else if(i == sequence.size()-1){
-                return true;
             }
         }
-        return false;
+        return i == sequence.size();
+    }
+
+    public static boolean isValidSubsequenceAlternative(List<Integer> array, List<Integer> sequence) {
+
+        if(sequence.size() > array.size()){
+            return false;
+        }
+
+       int seqIdx = 0 , arrayIdx = 0;
+       while (seqIdx < sequence.size() && arrayIdx < array.size()){
+           if (seqIdx == sequence.size()){
+               break;
+           }
+           if (array.get(arrayIdx).equals(sequence.get(seqIdx))){
+               seqIdx++;
+           }
+           arrayIdx++;
+       }
+
+       return seqIdx == sequence.size();
     }
 }
